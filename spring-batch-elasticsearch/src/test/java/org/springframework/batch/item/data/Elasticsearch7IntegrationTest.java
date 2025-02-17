58536da8257a75fc791e2b8743757f7df2dfa751
Henning Pöttker <25299532+hpoettker@users.noreply.github.com>
2025-02-17 01:42:22 +0100

    Integration tests for writer

--- spring-batch-elasticsearch/src/test/java/org/springframework/batch/item/data/Elasticsearch7IntegrationTest.java
+++ spring-batch-elasticsearch/src/test/java/org/springframework/batch/item/data/Elasticsearch7IntegrationTest.java
@@ -1,17 +1,42 @@
 package org.springframework.batch.item.data;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.ResourcelessJobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -20,18 +45,104 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.batch.item.data.TestDatabase.ELASTICSEARCH_7;
 
 @Testcontainers
-@SpringJUnitConfig
-@ImportAutoConfiguration({ ElasticsearchRestClientAutoConfiguration.class, ElasticsearchClientAutoConfiguration.class })
+@SpringBatchTest
+@ImportAutoConfiguration({ ElasticsearchRestClientAutoConfiguration.class, ElasticsearchClientAutoConfiguration.class,
+		ElasticsearchDataAutoConfiguration.class })
+@TestPropertySource(properties = { "spring.elasticsearch.username=elastic", "spring.elasticsearch.password=changeme" })
 class Elasticsearch7IntegrationTest {
 
 	@Container
-	@ServiceConnection
-	static ElasticsearchContainer container = new ElasticsearchContainer(ELASTICSEARCH_7.getImageName());
+	static ElasticsearchContainer container = new ElasticsearchContainer(ELASTICSEARCH_7.getImageName())
+		.withPassword("changeme");
+
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.elasticsearch.uris", () -> container.getHttpHostAddress());
+	}
 
 	@Test
 	void testSetup(@Autowired RestClient restClient) throws IOException {
 		Response response = restClient.performRequest(new Request("GET", "/_cluster/health"));
 		assertThat(response.getStatusLine().getStatusCode()).isEqualTo(200);
+	}
+
+	@Test
+	void testSpringData(@Autowired JobLauncherTestUtils jobLauncherTestUtils) throws Exception {
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+		assertThat(jobExecution).isNotNull();
+		assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+	}
+
+	@TestConfiguration
+	static class JobConfiguration {
+
+		@Bean
+		ResourcelessJobRepository resourcelessJobRepository() {
+			return new ResourcelessJobRepository();
+		}
+
+		@Bean
+		TaskExecutorJobLauncher jobLauncher(JobRepository jobRepository) {
+			var jobLauncher = new TaskExecutorJobLauncher();
+			jobLauncher.setJobRepository(jobRepository);
+			jobLauncher.setTaskExecutor(new SyncTaskExecutor());
+			return jobLauncher;
+		}
+
+		@Bean
+		Job job(JobRepository jobRepository, Step step) {
+			return new JobBuilder("job", jobRepository).start(step).build();
+		}
+
+		@Bean
+		Step step(JobRepository jobRepository, ItemProcessor<Integer, IndexQuery> processor,
+				ElasticsearchItemWriter writer) {
+			return new StepBuilder("step", jobRepository)
+				.<Integer, IndexQuery>chunk(10, new ResourcelessTransactionManager())
+				.reader(new ListItemReader<>(IntStream.range(0, 100).boxed().toList()))
+				.processor(processor)
+				.writer(writer)
+				.build();
+		}
+
+		@Bean
+		ItemProcessor<Integer, IndexQuery> itemProcessor() {
+			return i -> {
+				var item = new Elasticsearch8IntegrationTest.TestItem(String.valueOf(i));
+				return new IndexQueryBuilder().withObject(item).build();
+			};
+		}
+
+		@Bean
+		ElasticsearchItemWriter elasticsearchItemWriter(ElasticsearchOperations elasticsearchOperations) {
+			var indexCoordinates = elasticsearchOperations
+				.getIndexCoordinatesFor(Elasticsearch8IntegrationTest.TestItem.class);
+			return new ElasticsearchItemWriter(elasticsearchOperations, indexCoordinates);
+		}
+
+	}
+
+	@Document(indexName = "test-index")
+	static class TestItem {
+
+		@Id
+		private String id;
+
+		private String name;
+
+		TestItem(String name) {
+			this.name = name;
+		}
+
+		String getId() {
+			return id;
+		}
+
+		String getName() {
+			return id;
+		}
+
 	}
 
 }
--- spring-batch-elasticsearch/src/test/java/org/springframework/batch/item/data/Elasticsearch8IntegrationTest.java
+++ spring-batch-elasticsearch/src/test/java/org/springframework/batch/item/data/Elasticsearch8IntegrationTest.java
@@ -1,6 +1,7 @@
 package org.springframework.batch.item.data;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.Request;
@@ -8,17 +9,37 @@ import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.ResourcelessJobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -27,8 +48,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.batch.item.data.TestDatabase.ELASTICSEARCH_8;
 
 @Testcontainers
-@SpringJUnitConfig
-@ImportAutoConfiguration({ ElasticsearchRestClientAutoConfiguration.class, ElasticsearchClientAutoConfiguration.class })
+@SpringBatchTest
+@ImportAutoConfiguration({ ElasticsearchRestClientAutoConfiguration.class, ElasticsearchClientAutoConfiguration.class,
+		ElasticsearchDataAutoConfiguration.class })
 @TestPropertySource(properties = { "spring.elasticsearch.username=elastic", "spring.elasticsearch.password=changeme" })
 class Elasticsearch8IntegrationTest {
 
@@ -46,8 +68,59 @@ class Elasticsearch8IntegrationTest {
 		assertThat(response.getStatusLine().getStatusCode()).isEqualTo(200);
 	}
 
+	@Test
+	void testSpringData(@Autowired JobLauncherTestUtils jobLauncherTestUtils) throws Exception {
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+		assertThat(jobExecution).isNotNull();
+		assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+	}
+
 	@TestConfiguration
-	static class Ssl {
+	static class JobConfiguration {
+
+		@Bean
+		ResourcelessJobRepository resourcelessJobRepository() {
+			return new ResourcelessJobRepository();
+		}
+
+		@Bean
+		TaskExecutorJobLauncher jobLauncher(JobRepository jobRepository) {
+			var jobLauncher = new TaskExecutorJobLauncher();
+			jobLauncher.setJobRepository(jobRepository);
+			jobLauncher.setTaskExecutor(new SyncTaskExecutor());
+			return jobLauncher;
+		}
+
+		@Bean
+		Job job(JobRepository jobRepository, Step step) {
+			return new JobBuilder("job", jobRepository).start(step).build();
+		}
+
+		@Bean
+		Step step(JobRepository jobRepository, ItemProcessor<Integer, IndexQuery> processor,
+				ElasticsearchItemWriter writer) {
+			return new StepBuilder("step", jobRepository)
+				.<Integer, IndexQuery>chunk(10, new ResourcelessTransactionManager())
+				.reader(new ListItemReader<>(IntStream.range(0, 100).boxed().toList()))
+				.processor(processor)
+				.writer(writer)
+				.build();
+		}
+
+		@Bean
+		ItemProcessor<Integer, IndexQuery> itemProcessor() {
+			return i -> {
+				var item = new TestItem(String.valueOf(i));
+				return new IndexQueryBuilder().withObject(item).build();
+			};
+		}
+
+		@Bean
+		ElasticsearchItemWriter elasticsearchItemWriter(ElasticsearchOperations elasticsearchOperations) {
+			var indexCoordinates = elasticsearchOperations.getIndexCoordinatesFor(TestItem.class);
+			return new ElasticsearchItemWriter(elasticsearchOperations, indexCoordinates);
+		}
 
 		@Bean
 		public RestClientBuilderCustomizer customizer() {
@@ -62,6 +135,28 @@ class Elasticsearch8IntegrationTest {
 					builder.setSSLContext(container.createSslContextFromCa());
 				}
 			};
+		}
+
+	}
+
+	@Document(indexName = "test-index")
+	static class TestItem {
+
+		@Id
+		private String id;
+
+		private String name;
+
+		TestItem(String name) {
+			this.name = name;
+		}
+
+		String getId() {
+			return id;
+		}
+
+		String getName() {
+			return id;
 		}
 
 	}
